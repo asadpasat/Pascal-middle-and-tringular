dfacd0399d3860c1a3a979ca0ef61aa0b4e775bd
Asad Pasat <asadpasat@asads-mbp.attlocal.net>
2015-07-20 21:10:44 -0700

    python program to compare the middle number to triangular diagonal

---
+++ trigandmiddle.py
@@ -0,0 +1,24 @@
+"""finding number that appears in both sequences of
+middle number and triangular number
+"""
+import math
+
+person = input("what is your high band?")
+
+#triangular numbers
+def trig(x): return ((x**2 + x)/2)
+a= map(trig, range(1,person))
+
+#middle numbers
+def mid(z): return ((math.factorial(z))/(math.factorial(z/2)**2))
+b = map(mid, range (0,person,2))
+
+#tetrahedral numbers
+#def tetra(c): return (((c**3) + 3*(c**2) + (2*c))/ 6)
+#c = map(tetra, range (1,person))
+
+#pentatope numbers 
+#def penta(v): return ((v(v+1)(v+2)(v+5))/2)
+#v = map(penta, range (1,person))
+
+print(([i for j, i in zip(a,c) if i == j]))
